3792831b4ad93c3df2e3acd1a2bfd676cf6b0bcf
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-01-03 20:57:35 -0700

    commit tree 7c7acf60f4a4b9154352ebc893071b845ff02f4e

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 2, 'M', 'Brown', 'Happy', 'yes'),
+('Rowdy', 3, 'M', 'Black', 'Happy', 'yes'),
+('Tabitha', 5, 'F', 'Black', 'Mean', 'yes'),
+('Sergeant Brown', 1, 'M', 'Brown', 'Happy', 'yes'),
+('Melissa', 2, 'F', 'Black', 'Happy', 'yes'),
+('Grinch', 7, 'M', 'Black', 'Mean', 'yes'),
+('Wendy', 3, 'M', 'Black', 'Happy', 'yes'),
+(null, 6, 'M', 'White', 'Confused', 'yes');
